4c045b45a5a31b3ad9a6da8ef9bf9a8698581ef5
Haruki Koga <harukikoga.dev@gmail.com>
2023-01-01 23:23:11 +0900

    first commit

---
+++ AiChat/AiChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiChatApp.swift
+//  AiChat
+//
+//  Created by 古賀遥貴 on 2023/01/01.
+//
+
+import SwiftUI
+
+@main
+struct AiChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
